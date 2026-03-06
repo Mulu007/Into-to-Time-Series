8352856c505d6140eed0b023d9c7b29a29909d0a
Silvanius <110171336+Mulu007@users.noreply.github.com>
2026-03-05 21:20:39 -0600

    Addition of '3.4 Evaluating forecast accuracy

--- Intro to T-S.R
+++ Intro to T-S.R
@@ -194,3 +194,70 @@ Box.test(res, lag=10)
 # Box-Ljung test
 Box.test(res,lag=10, type="Lj")
 
+# 3.4 Evaluating forecast accuracy
+# Functions to subset a time-series
+window(ausbeer, start=1995)
+
+# subset()
+# substract 4*5 (20) quarters -> 5 years. 
+# grabs last 5 years
+subset(ausbeer, start = length(ausbeer)-4*5)
+tail(ausbeer, 4*5)
+
+# grabs first quarter of every year -> useful for seasonal analysis
+subset(ausbeer, quarter = 1)
+
+beer2 <- window(ausbeer, start = 1992, end = c(2007, 4))
+beerfit1 <- meanf(beer2, h = 10)
+beerfit2 <- rwf(beer2, h = 10)
+beerfit3 <- snaive(beer2, h = 10)
+autoplot(window(ausbeer, start = 1992)) +
+  autolayer(beerfit1, series = "Mean", PI = FALSE) +
+  autolayer(beerfit2, series = "Naive", PI = FALSE) +
+  autolayer(beerfit3, series = "Seasonal naive", PI = FALSE) +
+  xlab("Year") + ylab("Megalitres") +
+  ggtitle("Forecasts for quarterly beer production") +
+  guides(colour = guide_legend(title = "Forecast"))
+
+beer3 <- window(ausbeer, start=2008)
+accuracy(beerfit1, beer3)
+accuracy(beerfit2, beer3)
+accuracy(beerfit3, beer3)
+
+googfc1 <- meanf(goog200, h=40)
+googfc2 <- rwf(goog200, h=40)
+googfc3 <- rwf(goog200, drift=TRUE, h=40)
+autoplot(subset(goog, end = 240)) +
+  autolayer(googfc1, PI=FALSE, series="Mean") +
+  autolayer(googfc2, PI=FALSE, series="Naïve") +
+  autolayer(googfc3, PI=FALSE, series="Drift") +
+  xlab("Day") + ylab("Closing Price (US$)") +
+  ggtitle("Google stock price (daily ending 6 Dec 13)") +
+  guides(colour=guide_legend(title="Forecast"))
+
+googtest <- window(goog, start=201, end=240)
+accuracy(googfc1, googtest)
+accuracy(googfc2, googtest)
+accuracy(googfc3, googtest)
+
+#Time series cross validation
+e <- tsCV(goog200, rwf, drift=TRUE, h=1)
+sqrt(mean(e^2, na.rm=TRUE))
+#> [1] 6.233
+sqrt(mean(residuals(rwf(goog200, drift=TRUE))^2, na.rm=TRUE))
+#> [1] 6.169
+
+# Pipe operator
+goog200 %>% tsCV(forecastfunction=rwf, drift=TRUE, h=1) -> e
+e^2 %>% mean(na.rm=TRUE) %>% sqrt()
+#> [1] 6.233
+goog200 %>% rwf(drift=TRUE) %>% residuals() -> res
+res^2 %>% mean(na.rm=TRUE) %>% sqrt()
+#> [1] 6.169
+
+e <- tsCV(goog200, forecastfunction=naive, h=8)
+# Compute the MSE values and remove missing values
+mse <- colMeans(e^2, na.rm = T)
+# Plot the MSE values against the forecast horizon
+data.frame(h = 1:8, MSE = mse) %>%
+  ggplot(aes(x = h, y = MSE)) + geom_point()
